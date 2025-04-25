9021fcf2c8790af3f92ac6d6f1329ea714681633
trivopia <135800895+trivopia@users.noreply.github.com>
2025-04-25 20:57:32 +0700

    modified:   index.html modified:   main.js modified:   styles.css
    Added 3 functions
    1. addDiv 2. hover 3. click
    
    Need to revise the click function.

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
-function addDiv (n) {
-  
+addDiv(10);
+hover();
+click();
+
+function addDiv(n) {
+  let container = document.querySelector(".container");
+
+  for (let i = 0; i < n * n; i++) {
+    let newSquare = document.createElement("div");
+    newSquare.classList.add("square");
+
+    newSquare.style.width = `${40 / n}rem`;
+    newSquare.style.height = `${40 / n}rem`;
+    container.append(newSquare);
+  }
+}
+
+function hover() {
+  let squares = document.querySelectorAll(".square");
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function () {
+      squares[i].classList.add("hover");
+    });
+
+    squares[i].addEventListener("mouseout", function () {
+      setTimeout(() => {
+        squares[i].classList.remove("hover");
+      }, 50);
+    });
+  }
+}
+
+function click() {
+  let square = document.querySelectorAll(".square");
+  square.forEach(function (square) {
+    square.addEventListener("mousedown", function () {
+      square.classList.add("clicked");
+    });
+  });
 }
