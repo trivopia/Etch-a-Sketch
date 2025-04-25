6378408cba5567f433c7cd084dbe828ac969c74a
trivopia <135800895+trivopia@users.noreply.github.com>
2025-04-25 22:44:41 +0700

    modified:   main.js modified:   styles.css
    Substituted the previous hover function to CSS hover for simplicity
    
    Created paint() function. Used event delegation

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 addDiv(10);
-hover();
-click();
+paint();
 
 function addDiv(n) {
   let container = document.querySelector(".container");
@@ -15,6 +14,58 @@ function addDiv(n) {
   }
 }
 
+function paint() {
+  let mouseState;
+  let container = document.querySelector(".container");
+
+  container.addEventListener("mousedown", function (event) {
+    mouseState = true;
+    if (event.target.classList.contains("square")) {
+      event.target.classList.add("painted");
+    }
+  });
+
+  document.addEventListener("mouseup", function () {
+    mouseState = false;
+  });
+
+  container.addEventListener("mouseover", function (event) {
+    if (mouseState && event.target.classList.contains("square")) {
+      event.target.classList.add("painted");
+    }
+  });
+}
+
+/** old paint function 
+*
+function paint() {
+  let mouseState;
+
+  document.addEventListener("mousedown", function () {
+    mouseState = true;
+  });
+  document.addEventListener("mouseup", function () {
+    mouseState = false;
+  });
+
+  let container = document.querySelector(".container");
+
+  container.addEventListener("mousedown", function (event) {
+    if (event.target.classList.contains("square")) {
+      event.target.classList.add("painted");
+    }
+  });
+
+  container.addEventListener("mouseover", function (event) {
+    if (mouseState && event.target.classList.contains("square")) {
+      event.target.classList.add("painted");
+    }
+  });
+}
+ */
+
+/** hover function without event delegation
+*
 function hover() {
   let squares = document.querySelectorAll(".square");
   for (let i = 0; i < squares.length; i++) {
@@ -29,12 +80,25 @@ function hover() {
     });
   }
 }
+*/
 
-function click() {
-  let square = document.querySelectorAll(".square");
-  square.forEach(function (square) {
-    square.addEventListener("mousedown", function () {
-      square.classList.add("clicked");
-    });
+/** hover function with event delegation
+*
+function hover() {
+  let container = document.querySelector(".container");
+
+  container.addEventListener("mouseover", function (event) {
+    if (event.target.classList.contains("square")) {
+      event.target.classList.add("hover");
+    }
+  });
+
+  container.addEventListener("mouseout", function (event) {
+    if (event.target.classList.contains("square")) {
+      setTimeout(() => {
+        event.target.classList.remove("hover");
+      }, 100);
+    }
   });
 }
+*/
